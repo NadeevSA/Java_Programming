e5b0d9c0eedaaa7b79a1e84ed4394d8dc5e4f736
BespoyasovaVA <v.markutsaa@mail.ru>
2023-11-10 16:21:38 +0500

    марафон
    - изменить вызов методов
    
    решено

--- lab2/src/main/java/org/example/Main.java
+++ lab2/src/main/java/org/example/Main.java
@@ -1,23 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+
 /**
  * @author BespoyasovaV
  */
 public class Main {
+
+
     public static void main(String[] args) {
         Animal dog1 = new Dog("dog1");
-        System.out.println(dog1.jump(1));
-        // с ограничениями
         Animal dog2 = new Dog("dog2", 0, 50, 0.8);
-        System.out.println(dog2.jump(0.7));
         Animal dog3 = new Dog("dog3", 0, 50, 0.6);
-        System.out.println(dog3.jump(0.7));
 
         Animal cat1 = new Cat("cat1");
-        System.out.println(cat1.swim(100));
+
 
         Animal cat2 = new Cat("cat2");
-        System.out.println(cat2.jump(0.1));
 
+        Animal[] animals = {dog1, dog2, dog3, cat1, cat2};
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i].jump(0.8));
+            System.out.println(animals[i].swim(2));
+            System.out.println(animals[i].run(100));
+        }
     }
 }
