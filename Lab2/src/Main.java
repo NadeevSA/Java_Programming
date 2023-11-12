07fd3ec8bc8f567b007bf2eca39fca8dce75da2a
bbbtraveler <ildar-tukaev@mail.ru>
2023-11-12 21:49:49 +0500

    Марафон

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -11,10 +11,11 @@ public class Main {
         animals.add(new Cat("Cat2"));
         animals.add(new Cat("Cat3", 250,1.4));
 
+        var runDistance = 400;
+        var jumpDistance = 1.5;
+        var swimDistance = 10;
+
         for (var animal: animals) {
-            var runDistance = 400;
-            var jumpDistance = 1.5;
-            var swimDistance = 10;
             System.out.println(animal.getName() + ".run(" + runDistance + ");->результат: run: " + animal.run(runDistance));
             System.out.println(animal.getName() + ".jump(" + jumpDistance + ");->результат: jump: " + animal.jump(jumpDistance));
             System.out.println(animal.getName() + ".swim(" + swimDistance + ");->результат: swim: " + animal.jump(swimDistance));
